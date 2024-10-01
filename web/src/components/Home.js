3c3bcd2116c06f2373c47f0cf89fc3413c46ba3a
Yixuan Li <elainelyx114@gmail.com>
2024-09-30 22:18:19 -0400

    update template

---
+++ web/src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to Elaine's Website</h1>
+      <p>Different color represents different aspects of me.</p>
+    </div>
+  );
+}
+
+export default Home;

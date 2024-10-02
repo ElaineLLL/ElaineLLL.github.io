ae33257f15efa89ab5d20203f89e3f9d8a434761
Yixuan Li <elainelyx114@gmail.com>
2024-10-02 00:05:23 -0400

    upadate backend

--- Server/app.py
+++ Server/app.py
@@ -1,34 +1,14 @@
-from flask import Flask, send_from_directory, jsonify
-import os
+from flask import Flask, jsonify
 
-app = Flask(__name__, static_folder='../Web')
+app = Flask(__name__)
 
-# Serve the index.html file
-@app.route('/')
-def serve_index():
-    return send_from_directory(app.static_folder, 'index.html')
+@app.route('/api/blue', methods=['GET'])
+def blue():
+    return jsonify({"message": "Hello, the color is blue!"})
 
-# Serve static files (CSS, JS, images, etc.)
-@app.route('/<path:path>')
-def serve_static_files(path):
-    if os.path.exists(os.path.join(app.static_folder, path)):
-        return send_from_directory(app.static_folder, path)
-    else:
-        return send_from_directory(app.static_folder, 'index.html')
-
-# API endpoint
-@app.route('/api/empty')
-def get_message():
-    return jsonify(message="This color does not represent me...")
-
-#  test
-@app.route('/api/blue')
-def pink_message():
-    return jsonify(message="You chose blue!")
-
-@app.route('/api/message')
-def message():
-    return jsonify(message="Click!")
+@app.route('/api/empty', methods=['GET'])
+def empty():
+    return jsonify({"message": "This color does not belong to me..."})
 
 if __name__ == '__main__':
-    app.run(port=3000, debug=True)
+    app.run(debug=True)
